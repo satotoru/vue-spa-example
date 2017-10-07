3b41ce538b13f490449130e609fb953f3657d995
satotoru <satotoru2000@gmail.com>
2017-10-07 16:57:35 +0900

    get XY coordinate on click

--- src/components/Coordinate.vue
+++ src/components/Coordinate.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="coordinate">
     <router-link to="/">トップ</router-link>
+    <div class="box" ref="box" @click="onClick">
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'coordinate',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    onClick: function(e) {
+      const boxRect = this.$refs.box.getBoundingClientRect()
+      const clickRect = {
+        x: e.clientX - boxRect.left,
+        y: e.clientY - boxRect.top,
+      }
     }
   }
 }
 </script>
+
+<style lang="scss">
+  .box {
+    height: 500px;
+    width: 500px;
+    background-color:lightgray;
+    margin: auto;
+  }
+</style>
