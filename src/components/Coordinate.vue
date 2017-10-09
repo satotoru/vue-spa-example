f976791b3ac6e87dc159bb5bb4cfd1d917b930ac
satotoru <satotoru2000@gmail.com>
2017-10-09 15:30:51 +0900

    pin icon

--- src/components/Coordinate.vue
+++ src/components/Coordinate.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="coordinate">
     <div class="box" ref="box" @click="onClick">
+      <img v-for="(item, index) in pinnedItems" :key="index" src="../assets/pin_icon.png" alt="pin" class="icon" :style="getIconStyle(item)">
     </div>
     <b-modal title="送信ダイアログ" v-model="dialogOpen" @ok="onSubmit">
       <b-form-select v-model="selectedValue" :options="options" class="mb-3">
@@ -16,6 +17,7 @@ export default {
   name: 'coordinate',
   data() {
     return {
+      pinnedItems: [],
       dialogOpen: false,
       clickRect: { x: 0, y: 0 },
       selectedValue: null,
@@ -27,6 +29,16 @@ export default {
     }
   },
   methods: {
+    getIconStyle: function(pinnedItem) {
+      const width = 30;
+      const height = 30;
+      return {
+        width: `${width}px`,
+        height: `${height}px`,
+        top: `${pinnedItem.y - (height / 2)}px`,
+        left: `${pinnedItem.x - (width / 2)}px`,
+      }
+    },
     onClick: function(e) {
       const boxRect = this.$refs.box.getBoundingClientRect()
       this.clickRect = {
@@ -40,7 +52,7 @@ export default {
       axios.post("/api", {
         clickRect, selectedValue
       }).then(({ data }) => {
-        window.alert(data.message)
+        this.pinnedItems.push(this.clickRect)
         this.clickRect = { x: 0, y: 0 }
         this.selectedValue = null
       })
@@ -55,5 +67,9 @@ export default {
     width: 500px;
     background-color:lightgray;
     margin: auto;
+    position: relative;
+  }
+  .icon {
+    position: absolute;
   }
 </style>
